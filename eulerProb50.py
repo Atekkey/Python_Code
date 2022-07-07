84ecd0f65fc8492310b30c251230540eb80f75bb
Atekkey <anand@tekkey.net>
2022-07-07 00:31:14 -0400

    prob50

--- eulerProb50.py
+++ eulerProb50.py
@@ -1,9 +1,12 @@
 from Prime import *
 import time
+import math
 #print(primes_top(100))
-def largConsecCount(num):
+def largConsecCount(num, Q):
+    if(Q==0):
+        Q = int(num/20)
     pList, countMax, sumCur, countCur, bestPrime = primes_top(num), 0, 0, 0, 0
-    for i in range(len(pList)-1):
+    for i in range(int((1/Q)*len(pList))):
         countCur = 0
         sumCur = 0
         for k in range(i, len(pList)):
@@ -13,18 +16,20 @@ def largConsecCount(num):
                 if(countCur > countMax): 
                     countMax = countCur
                     bestPrime = sumCur
-    return countMax, bestPrime
+    return countMax, bestPrime, Q, num
 st = time.time()
-print(largConsecCount(8000))
+tuple = largConsecCount(10000, 0)
+#Q is a coeff that might make it more efficient.  range( int((1/Q)*len(pList)-1) )
+#Q=30 has proven to work for 1k-->10k . Same with Q = 4*math.log(num)
+#
 et = time.time()
-print(f"Run Time {str(et-st)[0:6]} sec")
+print(f"\nCeil: {tuple[3]} | Prime: {tuple[1]} | Consec: {tuple[0]} | Run Time {str(et-st)[0:6]} sec | Q = {tuple[2]}\n")
+
 """
-Num // AVG Time
-1k     0.0371
-5k     2.1
-10k   12.8
-15k   36.7
-20k   80.9
-25k  145.5
-30k  238.4
+Ceil: 1000 | Prime: 953 | Consec: 21 | Run Time 0.0044 sec | Q = 29
+Ceil: 5000 | Prime: 4651 | Consec: 45 | Run Time 0.1611 sec | Q = 36
+Ceil: 10000 | Prime: 9521 | Consec: 65 | Run Time 0.7866 sec | Q = 40
+Ceil: 30000 | Prime: 28697 | Consec: 108 | Run Time 2.2095 sec | Q = 600.0
+Ceil: 40000 | Prime: 38921 | Consec: 124 | Run Time 3.1374 sec | Q = 2000
+
 """
