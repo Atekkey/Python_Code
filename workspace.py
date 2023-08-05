e85e86827c68d2b9eb67facf3ed64d4f67479837
Atekkey <anand@tekkey.net>
2023-08-05 16:52:47 -0500

    aug

--- workspace.py
+++
@@ -1,8 +0,0 @@
-play1 = input("Player one Choice (R/P/S)")
-play2 = input("Player one Choice (R/P/S)")
-if(play1 == play2):
-    print("Its a tie!")
-if( (play1 == "R" and play2 == "S")  or (play1 == "P" and play2 == "R")   or  (play1 == "S" and play2 == "P")):
-    print("Player 1 wins!")
-else:
-    print("Player 2 wins!")
